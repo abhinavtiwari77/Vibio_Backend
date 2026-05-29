87516c2dc032fd36d47fd6b9afa140d7728ff7df
abhinavt77 <abhinavt00001@gmail.com>
2026-05-29 16:58:27 +0530

    Fix Linux case-sensitive model imports

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,5 @@
 const Post = require("../models/Post");
-const User = require("../models/user");
+const User = require("../models/User");
 const { uploadBufferToCloudinary, cloudinary } = require("../utils/upload");
 
 function parseMediaEdits(input) {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/User");
 const bcrypt = require("bcrypt");
 const { uploadBufferToCloudinary, cloudinary } = require("../utils/upload");
 const Notification = require("../models/Notification");
